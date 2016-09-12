ea3d4c1cac90b4903d42ec042fe0afa63dbee530
inky <ethanbrews@icloud.com>
2016-09-12 20:38:20 +0100

    Testing

---
+++ src/main/java/uk/co/conclipsegames/projDart/Constants.java
@@ -0,0 +1,7 @@
+package uk.co.conclipsegames.projDart;
+
+public class Constants {
+    public static final String MODID = "projdart";
+    public static final String MODNAME = "Project Dart";
+    public static final String VERSION = "1.0";
+}
---
+++ src/main/java/uk/co/conclipsegames/projDart/ProjectDart.java
@@ -0,0 +1,7 @@
+package uk.co.conclipsegames.projDart;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(name = Constants.MODNAME, version = Constants.VERSION, modid = Constants.MODID, )
+public class ProjectDart {
+}
