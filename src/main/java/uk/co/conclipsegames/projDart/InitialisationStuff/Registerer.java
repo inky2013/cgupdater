3bfd574fcc1160731e5336434e80205590d222e0
inky <ethanbrews@icloud.com>
2016-09-13 20:26:22 +0100

    Added function to initialize items

--- src/main/java/uk/co/conclipsegames/projDart/InitialisationStuff/Registerer.java
+++ src/main/java/uk/co/conclipsegames/projDart/InitialisationStuff/Registerer.java
@@ -1,9 +1,13 @@
 package uk.co.conclipsegames.projDart.InitialisationStuff;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+import uk.co.conclipsegames.projDart.Constants;
+
 public class Registerer {
 
-    public void registerItem() {
-        
+    public static void registerItem(Item item){
+        GameRegistry.registerItem(item, Constants.MODID + " " + item.getUnlocalizedName().substring(5));
     }
 
 }
