e67045e12bd6f1122c1322ee830019d463c58fce
inky <ethanbrews@icloud.com>
2016-09-13 20:27:45 +0100

    Added register items

--- src/main/java/uk/co/conclipsegames/projDart/proxy/CommonProxy.java
+++ src/main/java/uk/co/conclipsegames/projDart/proxy/CommonProxy.java
@@ -3,11 +3,13 @@ package uk.co.conclipsegames.projDart.Proxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import uk.co.conclipsegames.projDart.Item.Materials;
 
 public class CommonProxy {
 
     public void preInit(FMLPreInitializationEvent e) {
         System.out.println("I'm a mod");
+        Materials.registerItems();
     }
     public void init(FMLInitializationEvent e) {
 
