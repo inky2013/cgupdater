ec6a0c7d341167bfeb55251dfd05ba4d1dc18a0e
JSJBDEV <acrodynasty@gmail.com>
2016-09-13 20:11:31 +0100

    added force gem

--- src/main/java/uk/co/conclipsegames/projDart/Item/Items.java
+++ src/main/java/uk/co/conclipsegames/projDart/Item/Items.java
@@ -1,11 +1,14 @@
 package uk.co.conclipsegames.projDart.Item;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class Items {
     public static Item forceGem;
 
     public static final void init() {
-        forceGem = new Item();
+        forceGem = new Item().setUnlocalizedName("Force Gem").setCreativeTab(CreativeTabs.tabMisc);
+        GameRegistry.registerItem(forceGem, "forceGem");
     }
 }
