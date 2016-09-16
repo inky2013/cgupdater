c85e790d9130511a8040b996495eef14ece659c3
inky <ethanbrews@icloud.com>
2016-09-16 18:38:44 +0100

    Added recipes

--- src/main/java/uk/co/conclipsegames/projDart/Afterthoughts/PDCrafting.java
+++ src/main/java/uk/co/conclipsegames/projDart/Afterthoughts/PDCrafting.java
@@ -1,6 +1,7 @@
 package uk.co.conclipsegames.projDart.Afterthoughts;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import uk.co.conclipsegames.projDart.Item.PDItems;
 
@@ -8,7 +9,10 @@ public class PDCrafting {
 
     public static void init() {
 
-        GameRegistry.addShapelessRecipe(new ItemStack(PDItems.itemIngot));
+        GameRegistry.addShapelessRecipe(new ItemStack(PDItems.itemIngot, 2, 1), new Object[] {Items.gold_ingot, Items.gold_ingot, PDItems.forceGem});
+        GameRegistry.addShapelessRecipe(new ItemStack(PDItems.itemIngot, 1, 1), new Object[] {Items.gold_ingot, PDItems.forceGem});
+        GameRegistry.addShapelessRecipe(new ItemStack(PDItems.itemIngot, 1, 1), new Object[] {Items.iron_ingot, Items.iron_ingot, PDItems.forceGem});
+        GameRegistry.addSmelting(new ItemStack(PDItems.itemDust, 1, 1), new ItemStack(PDItems.itemIngot, 1, 1), 4F);
 
     }
 }
