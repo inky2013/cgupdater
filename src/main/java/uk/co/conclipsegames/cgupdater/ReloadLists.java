381a16b7a1be3b47f2728ec6ee6816d426932e3d
inky2013 <ethanbrews77@gmail.com>
2016-09-24 23:04:27 +0100

    Update ReloadLists.java

--- src/main/java/uk/co/conclipsegames/cgupdater/ReloadLists.java
+++ src/main/java/uk/co/conclipsegames/cgupdater/ReloadLists.java
@@ -11,7 +11,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.Pack200;
 
 public class UpdateCmd implements ICommand {
     private final List aliases;
@@ -19,7 +18,7 @@ public class UpdateCmd implements ICommand {
     public UpdateCmd()
     {
         aliases = new ArrayList();
-        aliases.add("update");
+        aliases.add("reloadupdates");
     }
 
     @Override
@@ -31,13 +30,13 @@ public class UpdateCmd implements ICommand {
     @Override
     public String getCommandName()
     {
-        return "update";
+        return "reloadupdates";
     }
 
     @Override
     public String getCommandUsage(ICommandSender var1)
     {
-        return "update <group> (groups are set in config)";
+        return "reloadupdates";
     }
 
     @Override
@@ -49,55 +48,7 @@ public class UpdateCmd implements ICommand {
     @Override
     public void processCommand(ICommandSender sender, String[] args)
     {
-        String url;
-        World world = sender.getEntityWorld();
-
-        if (!(world.isRemote)) {
-            if (args.length == 0) {
-                sender.addChatMessage(new ChatComponentText("Usage: /update <group> (groups are set in config)"));
-            }
-            for(int i=0;i< args.length; i++) {
-                url = CGUpdater.updateLoc.get(args[i]);
-                if (url == null) {
-                    sender.addChatMessage(new ChatComponentText(args[i]+" was not found in the config."));
-                } else {
-                    if (!(url.endsWith("/"))) { url = url+"/"; }
-                    url = url+"mission-control.list";
-                    URL u;
-                    try {
-                        u = new URL(url);
-                    } catch (MalformedURLException e) {
-                        sender.addChatMessage(new ChatComponentText(args[i]+" URL is invalid."));
-                        continue;
-                    }
-                    BufferedReader in;
-                    try {
-                        in = new BufferedReader(new InputStreamReader(u.openStream()));
-                    } catch (IOException e) {
-                        sender.addChatMessage(new ChatComponentText("Error while at mission control for " + args[i]));
-                        continue;
-                    }
-
-                    String inputLine;
-                    List<String> urls = new ArrayList<String>();
-                    try {
-                        while ((inputLine = in.readLine()) != null)
-                            urls.add(inputLine);
-                        in.close();
-                    } catch (IOException e) { sender.addChatMessage(new ChatComponentText("(IO)Error while at mission control for " + args[i])); }
-                    for (int y=0; y<urls.size(); y++) {
-                        try {
-                            FileUtils.copyURLToFile(new URL(CGUpdater.updateLoc.get(args[i]) + urls.get(y)), new File(CGUpdater.saveLoc.get(args[i]) + "/" + urls.get(y)));
-                        } catch (IOException ioe) {
-                            sender.addChatMessage(new ChatComponentText("Error Downloading "+urls.get(y)));
-                        }
-
-
-                    }
-                }
-
-            }
-        }
+        CGUpdater.something
     }
 
     @Override
